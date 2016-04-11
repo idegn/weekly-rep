b8eaff7b279d31723fd50d34367b087908e70ff7
idegn <genki.ide@gmail.com>
2016-04-11 16:22:51 +0900

    view修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,18 +2,24 @@ module ApplicationHelper
 
   def bs_submit(f)
     value = params[:action] == 'new' ? '作成' : '更新'
-    f.submit value, class: 'btn btn-primary'
-  end
-
-  def write_latest_report_btn
-    if current_user.wrote_latest_report?
-      link_to '週報を書く', new_weekly_report_path, class: 'btn btn-primary btn-lg'
-    end
+    f.submit value, class: 'btn btn-primary pull-right'
   end
 
   def gravatar_for(user)
-    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
     image_tag(gravatar_url, alt: user.name, class: 'gravatar')
+  end
+
+  def users_weekly_reports_path(user)
+    "/users/#{user.id}/weekly_reports"
+  end
+
+  def groups_weekly_reports_path(group)
+    "/groups/#{group.id}/weekly_reports"
+  end
+
+  def link_to_users_weekly_reports(user)
+    link_to user.name, users_weekly_reports_path(user), class: :user_name
   end
 end
