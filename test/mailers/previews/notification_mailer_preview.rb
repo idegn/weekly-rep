bf8d9b81c64cbdd8e1f528775cc193da7f5c7494
idegn <genki.ide@gmail.com>
2016-04-30 16:29:39 +0900

    メール修正とメールのpreview

--- test/mailers/previews/notification_mailer_preview.rb
+++ test/mailers/previews/notification_mailer_preview.rb
@@ -1,4 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/notification_mailer
 class NotificationMailerPreview < ActionMailer::Preview
-
+  def notification_mail
+    NotificationMailer.notification_mail(User.find(2))
+  end
 end
