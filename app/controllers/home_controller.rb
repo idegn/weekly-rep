2ffdaf038792abfa5495ed3e9a9bcadd70b259c3
idegn <genki.ide@gmail.com>
2016-04-30 17:10:50 +0900

    [from now] 2016/04/30 17:10:50
    diff --git a/app/controllers/home_controller.rb b/app/controllers/home_controller.rb
    index a208521..330e20d 100644
    --- a/app/controllers/home_controller.rb
    +++ b/app/controllers/home_controller.rb
    @@ -2,8 +2,7 @@ class HomeController < ApplicationController
       def index
         if user_signed_in? && current_user.group && current_user.approved
           @reports = current_user.group.weekly_reports.where(published: true).includes(:user)
    -      recent_time = @reports.maximum(:reported_time)
    -      @reports = @reports.where('reported_time = ?', recent_time).order('updated_at DESC')
    +      @reports = @reports.where('reported_time > ?', 1.week.ago).order('updated_at DESC')
         end
       end
     end
    diff --git a/app/views/home/index.html.erb b/app/views/home/index.html.erb
    index 50df8ce..c4fb9d3 100644
    --- a/app/views/home/index.html.erb
    +++ b/app/views/home/index.html.erb
    @@ -12,13 +12,12 @@
           <% end %>
         </tbody>
       </table>
    +<% elsif user_signed_in? %>
    +  <% if current_user.group.nil? %>
    +    <h1 class="text-center"><%= link_to '新しいグループを作る', new_group_path %></h1>
    +  <% else %>
    +    <h1 class="text-center"><%= link_to '週報を書こう！', new_weekly_report_path %></h1>
    +  <% end %>
     <% else %>
    -  <h1 class="text-center"><%= link_to '週報を書こう！', new_weekly_report_path %></h1>
    -<% end %>
    -<% if user_signed_in? %>
    -    <% if current_user.group.nil? %>
    -        <h1 class="text-center"><%= link_to '新しいグループを作る', new_group_path %></h1>
    -    <% end %>
    -<% else %>
    -    <h1 class="text-center index-links"><%= link_to "登録", new_user_registration_path %> or <%= link_to "ログイン", new_user_session_path %> or <%= link_to "die", 'https://www.google.co.jp/search?q=%E8%87%AA%E6%AE%BA+%E5%90%8D%E6%89%80&oq=%E8%87%AA%E6%AE%BA+%E5%90%8D%E6%89%80&aqs=chrome..69i57.2318j0j1&sourceid=chrome&ie=UTF-8' %></h1>
    +  <h1 class="text-center index-links"><%= link_to "登録", new_user_registration_path %> or <%= link_to "ログイン", new_user_session_path %> or <%= link_to "die", 'https://www.google.co.jp/search?q=%E8%87%AA%E6%AE%BA+%E5%90%8D%E6%89%80&oq=%E8%87%AA%E6%AE%BA+%E5%90%8D%E6%89%80&aqs=chrome..69i57.2318j0j1&sourceid=chrome&ie=UTF-8' %></h1>
     <% end %>

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,8 +2,7 @@ class HomeController < ApplicationController
   def index
     if user_signed_in? && current_user.group && current_user.approved
       @reports = current_user.group.weekly_reports.where(published: true).includes(:user)
-      recent_time = @reports.maximum(:reported_time)
-      @reports = @reports.where('reported_time = ?', recent_time).order('updated_at DESC')
+      @reports = @reports.where('reported_time > ?', 1.week.ago).order('updated_at DESC')
     end
   end
 end
