70535690e9d3ad94a38f4b70795ebbc484866e81
idegn <genki.ide@gmail.com>
2016-07-10 12:54:57 +0900

    fix a bug

--- app/models/weekly_report.rb
+++ app/models/weekly_report.rb
@@ -6,9 +6,9 @@ class WeeklyReport < ActiveRecord::Base
 
   def setup(current_user)
     if current_user.reported_this_week?
-      self.reported_time = current_user.group.reporting_time
+      self.reported_time = current_user.group.next_report_time
     else
-      self.reported_time = current_user.group.reporting_time - 1.week
+      self.reported_time = current_user.group.latest_report_time
     end
     self.content = current_user.group.template
   end
